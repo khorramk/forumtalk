4730474c49a7f980f61c3a53dc7d248708c09eed
Khorram Khondkar <khorramk.kbsk@gmail.com>
2024-04-22 17:25:41 +0100

    Topic initial setup with most models

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,13 @@ Route::get('/topics', function () {
         __('topics') => [
             __('religion') => [
                 __('discussion') => __('Religion came to sort out poeple problems'),
-                __('topic_rating_on_site') => 8
+                __('topic_rating_on_site') => 8,
+                __('written_by') => 'khorram'
             ],
             __('politics') =>  [
                 __('discussion') => __('War is not the solution to all problems'),
-                __('topic_rating_on_site') => 6
+                __('topic_rating_on_site') => 6,
+                __('written_by') => 'khorram'
             ]
         ],
     ]);
--- tests/Feature/TopicTest.php
+++ tests/Feature/TopicTest.php
@@ -2,8 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Models\Topic;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class TopicTest extends TestCase
@@ -22,12 +25,12 @@ class TopicTest extends TestCase
                 __('religion') => [
                     __('discussion') => __('Religion came to sort out poeple problems'),
                     __('topic_rating_on_site') => 8,
-                    __('wrtitten by') => 'khorram'
+                    __('written_by') => 'khorram'
                 ],
                 __('politics') =>  [
                     __('discussion') => __('War is not the solution to all problems'),
                     __('topic_rating_on_site') => 6,
-                    __('written by') => 'khorram'
+                    __('written_by') => 'khorram'
                 ]
             ]
         ]);
@@ -44,5 +47,41 @@ class TopicTest extends TestCase
 
     public function test_get_topic_from_database():void {
 
+        $user = new User();
+        $password = Hash::make('password124');
+        $user->fill(
+            [
+                'name' => 'khorram',
+                'email' => 'khorram@test.com',
+                'password' => $password
+            ]);
+
+
+        $user->save();
+
+        $topic =  new Topic();
+        $topic->fill([
+            'type' => 'religion',
+            'discussion' => 'religion is improve lives',
+            'topic_rating' => 8,
+            'user_id' => $user->id
+        ]);
+
+        $topic->save();
+
+
+        $this->assertDatabaseHas($topic, [
+            'type' => 'religion',
+            'discussion' => 'religion is improve lives',
+            'topic_rating' => 8,
+            'user_id' => $user->id
+        ]);
+
+
+        $this->assertDatabaseHas($user,  [
+            'name' => 'khorram',
+            'email' => 'khorram@test.com',
+            'password' => $password
+        ]);
     }
 }
