b09076b23d3d022c0d57f927f61435a0ec53c835
Khorram Khondkar <khorramk.kbsk@gmail.com>
2024-05-14 15:17:32 +0100

    Written migration schema: For comments Table and Reply table
    What it is: SQL table schema for comments and replies table
    Why: The reason being, comments table  and replies table needs data input and linking
    
     Co-authored-by: Khorram Khondkar github.com/khorramk

--- database/migrations/2024_05_14_114833_create_comments_table.php
+++ database/migrations/2024_05_14_114833_create_comments_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->string('comment')->nullable();
+            $table->bigInteger('topic_id')->nullable();
+            $table->bigInteger('user_id')->nullable();
             $table->timestamps();
         });
     }
--- database/migrations/2024_05_14_115601_create_replies_table.php
+++ database/migrations/2024_05_14_115601_create_replies_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('replies', function (Blueprint $table) {
             $table->id();
+            $table->string('reply')->nullable();
+            $table->bigInteger('comment_id')->nullable();
+            $table->bigInteger('user_id')->nullable();
             $table->timestamps();
         });
     }
